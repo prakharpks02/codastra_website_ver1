1a5778a3e7abf669d433e385dd49d006bf9c3d57
Prakhar <prakharpks02@gmail.com>
2020-09-17 19:19:37 +0530

    About page completed

--- static/js/index.js
+++ static/js/index.js
@@ -59,6 +59,35 @@ function CloseOverlay() {
 	document.getElementById("overlay-screen-container").classList.add("hide");
 }
 
-document.getElementById("overlay-screen").addEventListener("click",function(){
+document.getElementById("overlay-screen").addEventListener("click", function () {
 	CloseOverlay();
 })
+
+
+// Preparing SMTP mail sending
+
+function sendEmail() {
+	Email.send({
+		Host: "smtp.gmail.com",
+		Username: "sender@email_address.com",
+		Password: "Enter your password",
+		To: 'receiver@email_address.com',
+		From: "sender@email_address.com",
+		Subject: "Sending Email using javascript",
+		Body: "Well that was easy!!",
+		Attachments: [
+			{
+				name: "File_Name_with_Extension",
+				path: "Full Path of the file"
+			}]
+	})
+		.then(function (message) {
+			alert("Mail has been sent successfully")
+		});
+}
+
+// https://www.youtube.com/watch?v=V388-fC0qB8
+
+// https://www.youtube.com/watch?v=TtLZtKtNZ3M
+
+// 
